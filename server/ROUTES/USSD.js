154b73e1c6646f85e19176191cdafcd0c426cfa7
OddNavee <54965395+OddNavee@users.noreply.github.com>
2021-06-15 15:36:41 -0700

    OPTIMISATION.1

--- server/ROUTES/USSD.js
+++ server/ROUTES/USSD.js
@@ -3,7 +3,7 @@ const
     express = require('express'),
     // hash = require('md5'),
     client = require('../MODULES/database'),
-    SendMessage = require('../MODULES/twilio'),
+    SendMessage = require('../MODULES/SMS'),
     unique_id = require('../MODULES/uuid'),
     router = express.Router();
 
@@ -25,8 +25,7 @@ const
             if (inputs.length > 0) parseInt(inputs[0]) === 1 ? await UserInformation(Phonenumber) : parseInt(inputs[0]) === 2 ? await GenerateRandomPassword(Phonenumber) : response = `END Invalid Input.`
     },
     DisplayHomeScreen = async Phonenumber => await client.query(`SELECT * FROM patienttable`, (err, result) =>{
-        err ? console.log(err) :
-            result.rows.forEach( User => Phonenumber === User.phonenumber ? response = `CON Welcome to Digital Health Passport Mobile View. Select your action. \n\n 1. View and Edit User Information. \n 2. Generate Random Password.` : response = `END ${Phonenumber} is not a registered user.`)}
+        err ? console.log(err) : result.rows.forEach( User => Phonenumber === User.phonenumber ? response = `CON Welcome to Digital Health Passport Mobile View. Select your action. \n\n 1. View and Edit User Information. \n 2. Generate Random Password.` : response = `END ${Phonenumber} is not a registered user.`)}
     ),
     UserInformation = async Phonenumber =>{
         response = `CON 1. View Medical History. \n 2. View Personal Information. \n 3. Edit PIN.`
@@ -72,25 +71,24 @@ const
         }),
     UpdatePIN = async Phonenumber =>{
         response = `CON Enter Your Current PIN.`
-            if (inputs.length > 2){
-                await client.query(`SELECT * FROM patienttable WHERE phonenumber = '${Phonenumber}'`, (err, result) =>{
-                    err ? console.log(err) :
-                        result.rows.forEach( User =>{
-                            if (parseInt(inputs[2]) === parseInt(User.patientpassword)){
-                                response = `CON Enter Your New PIN.`
-                                    if (inputs.length > 3){
-                                        if (parseInt(inputs[3].length) >= 4){
-                                            client.query(`UPDATE patienttable SET patientpassword = '${inputs[3]}' WHERE phonenumber = '${Phonenumber}'`, err =>{
-                                                if (err) console.log(err)
-                                                    else {
-                                                        SendMessage(`PIN updated successfully. Your New PIN is ${inputs[3]}.`,`${Phonenumber}`)
-                                                            response = `END PIN Updated successfully. Your New PIN has been Sent to ${Phonenumber} via SMS.`
-                                                    }
-                                            })
-                                        } else if (parseInt(inputs[3].length) < 4) response = `END New PIN does not fit requirements. Try Again.`
-                                    }
-                            } else if (parseInt(inputs[2]) !== parseInt(User.patientpassword)) response = `END PIN MisMatch`
-                        })
-                })
-            }
+            if (inputs.length > 2) await client.query(`SELECT * FROM patienttable WHERE phonenumber = '${Phonenumber}'`, (err, result) =>{
+                err ? console.log(err) :
+                    result.rows.forEach( User =>{
+                        if (parseInt(inputs[2]) === parseInt(User.patientpassword)){
+                            response = `CON Enter Your New PIN.`
+                                if (inputs.length > 3){
+                                    if (parseInt(inputs[3].length) >= 4){
+                                        client.query(`UPDATE patienttable SET patientpassword = '${inputs[3]}' WHERE phonenumber = '${Phonenumber}'`, err =>{
+                                            if (err) console.log(err)
+                                                else {
+                                                    SendMessage(`PIN updated successfully. Your New PIN is ${inputs[3]}.`,`${Phonenumber}`)
+                                                        response = `END PIN Updated successfully. Your New PIN has been Sent to ${Phonenumber} via SMS.`
+                                                }
+                                        })
+                                    } else if (parseInt(inputs[3].length) < 4) response = `END New PIN does not fit requirements. Try Again.`
+                                }
+                        } else if (parseInt(inputs[2]) !== parseInt(User.patientpassword)) response = `END PIN MisMatch`
+                    })
+            })
+
     };
